a3fb23160d29cd0329cc57d3ee8a3b54468ebba5
thirdplay <thirdplay@outlook.com>
2017-01-04 18:15:51 +0900

    ログイン資材の追加

---
+++ source/CsDemo/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using CsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsDemo.Controllers
+{
+    /// <summary>
+    /// ログイン画面を制御するクラスです。
+    /// </summary>
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// 初期表示。
+        /// </summary>
+        /// <returns>アクションメソッドの結果</returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// ログイン処理。
+        /// </summary>
+        /// <param name="model">ログイン画面のViewModel</param>
+        /// <returns>アクションメソッドの結果</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // ログインユーザのチェック
+
+            // ユーザ情報画面に遷移
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
