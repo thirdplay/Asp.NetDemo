1f16401b07d11ae9796171e067c112f486cecf58
thirdplay <thirdplay@outlook.com>
2017-04-06 08:36:37 +0900

    ロガー設定の修正

--- projects/Prototype/Prototype/App_Start/LoggerConfig.cs
+++ projects/Prototype/Prototype/App_Start/LoggerConfig.cs
@@ -13,9 +13,8 @@ namespace Prototype
         /// </summary>
         public static void RegisterLoggers()
         {
-            log4net.GlobalContext.Properties["outputDir"] = @"D:\git\AspDotNetDemo\projects\Prototype\Commmon\Logs";//HostingEnvironment.MapPath("/Common/Logs");
-            log4net.Config.XmlConfigurator.Configure(new FileInfo(@"D:\git\AspDotNetDemo\projects\Prototype\Prototype\log4net.config"));
-            //log4net.Config.XmlConfigurator.Configure(new FileInfo(HostingEnvironment.MapPath("~/log4net.config")));
+            log4net.GlobalContext.Properties["outputDir"] = HostingEnvironment.MapPath("/Common/Logs");
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(HostingEnvironment.MapPath("~/log4net.config")));
         }
     }
 }
