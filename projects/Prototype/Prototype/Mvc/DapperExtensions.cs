f9365147c99cb5aa24dada2209c8ab352c96b6c1
thirdplay <thirdplay@outlook.com>
2017-05-01 00:54:36 +0900

    リファクタリング

--- projects/Prototype/Prototype/Mvc/DapperExtensions.cs
+++ projects/Prototype/Prototype/Mvc/DapperExtensions.cs
@@ -14,17 +14,17 @@ namespace Prototype.Mvc
         /// <summary>
         /// プリペアドステートメントで動的な検索条件を作成します。
         /// </summary>
-        /// <typeparam name="TSource">対象の値のシーケンス</typeparam>
-        /// <param name="list">パラメータリスト</param>
+        /// <typeparam name="TSource">要素の型</typeparam>
+        /// <param name="source">対象となる値のシーケンス</param>
         /// <param name="paramName">パラメータ名</param>
         /// <param name="condition">検索条件を返却するメソッド（引数:パラメータ名）</param>
         /// <param name="separator">区切り記号として使用する文字列</param>
         /// <param name="parameters">動的パラメータの格納先</param>
         /// <returns>検索条件の文字列</returns>
-        public static string CreateDynamicCondition<TSource>(this IEnumerable<TSource> list, string paramName, Func<string, string> condition, string separator, ref DynamicParameters parameters)
+        public static string CreateDynamicCondition<TSource>(this IEnumerable<TSource> source, string paramName, Func<string, string> condition, string separator, ref DynamicParameters parameters)
         {
             var result = new List<string>();
-            foreach (var value in list)
+            foreach (var value in source)
             {
                 var name = $"{paramName}{result.Count}";
                 result.Add(condition(name));
