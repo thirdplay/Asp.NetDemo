aae4c52f0475f15a8b7587202b789f53f97b71c0
thirdplay <thirdplay@outlook.com>
2017-05-15 23:09:59 +0900

    解析テスト

--- projects/Prototype/Prototype/Services/AnalyzableServiceBase.cs
+++ projects/Prototype/Prototype/Services/AnalyzableServiceBase.cs
@@ -17,130 +17,119 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Mvc;
+using NLog;
 
 namespace Prototype.Services
 {
-    /// <summary>
-    /// テスト解析サービスのインターフェース。
-    /// </summary>
-    public interface IAnalyzableServiceBase
-    {
-        /// <summary>
-        /// 解析処理。
-        /// </summary>
-        /// <param name="httpContext">HTTPコンテキスト</param>
-        /// <returns>解析結果のファイル名</returns>
-        Task<string> Analyze(HttpContextBase httpContext);
-    }
+	/// <summary>
+	/// テスト解析サービスのインターフェース。
+	/// </summary>
+	public interface IAnalyzableServiceBase
+	{
+		/// <summary>
+		/// 解析処理。
+		/// </summary>
+		/// <param name="httpContext">HTTPコンテキスト</param>
+		/// <returns>解析結果のファイル名</returns>
+		Task<string> Analyze(HttpContextBase httpContext);
+	}
 
-    /// <summary>
-    /// 解析サービスの基底クラス。
-    /// </summary>
-    public abstract class AnalyzableServiceBase : IAnalyzableServiceBase, IDisposable
-    {
-        /// <summary>
-        /// コンストラクタ。
-        /// </summary>
-        public AnalyzableServiceBase()
-        {
-            //this.Logger.Debug($"AnalyzableServiceBase:Constructor");
-        }
+	/// <summary>
+	/// 解析サービスの基底クラス。
+	/// </summary>
+	public abstract class AnalyzableServiceBase : IAnalyzableServiceBase, IDisposable
+	{
+		private Logger logger = LogManager.GetCurrentClassLogger();
 
-        /// <summary>
-        /// 解析処理。
-        /// </summary>
-        /// <param name="httpContext">HTTPコンテキスト</param>
-        /// <returns>解析結果のファイル名</returns>
-        public Task<string> Analyze(HttpContextBase httpContext)
-        {
-            return Task.Run(() =>
-            {
-                // InteropExcel
-                var resultDir = HostingEnvironment.MapPath("/Common/Result");
-                var tempDir = HostingEnvironment.MapPath("/Common/Template");
-                using (var excel = new InteropExcel(Path.Combine(tempDir, "prototype.xlsm")))
-                {
-                    excel.Run("ThisWorkbook.TestMacro4", "a1", "a2");
+		/// <summary>
+		/// コンストラクタ。
+		/// </summary>
+		public AnalyzableServiceBase()
+		{
+			//this.Logger.Debug($"AnalyzableServiceBase:Constructor");
+		}
 
-                    // 出力
-                    var fileName = "prototype_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsm";
-                    excel.SaveAs(Path.Combine(resultDir, fileName));
+		/// <summary>
+		/// 解析処理。
+		/// </summary>
+		/// <param name="httpContext">HTTPコンテキスト</param>
+		/// <returns>解析結果のファイル名</returns>
+		public Task<string> Analyze(HttpContextBase httpContext)
+		{
+			return Task.Run(() =>
+			{
+#if true
+				try
+				{
+					logger.Trace(">>解析開始");
+					for (var j = 0; j < 30; j++)
+					{
+						for (var i = 0; i < 60; i++)
+						{
+							Thread.Sleep(1000);
+						}
+						logger.Trace($"{(j + 1)}分経過");
+					}
+					logger.Trace(">>解析終了");
 
-                    return fileName;
-                }
-#if false
-                try
-                {
-                    Debug.WriteLine("ThreadId:" + Thread.CurrentThread.ManagedThreadId);
-                    Debug.WriteLine("SessionId:" + httpContext.Session.SessionID);
-                    Debug.WriteLine("CancelToken:" + httpContext.Session["CancelToken"]);
+					// InteropExcel
+					var resultDir = HostingEnvironment.MapPath("/Common/Result");
+					var tempDir = HostingEnvironment.MapPath("/Common/Template");
+					using (var excel = new InteropExcel(Path.Combine(tempDir, "prototype.xlsm")))
+					{
+						excel.Run("ThisWorkbook.TestMacro4", "a1", "a2");
 
-                    Debug.WriteLine("Step1:Processing...");
-                    Thread.Sleep(1000);
-                    Debug.WriteLine("Step1:Completed");
-                    token.ThrowIfCancellationRequested();
+						// 出力
+						var fileName = "prototype_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsm";
+						excel.SaveAs(Path.Combine(resultDir, fileName));
 
-                    Debug.WriteLine("Step2:Processing...");
-                    Thread.Sleep(1000);
-                    Debug.WriteLine("Step2:Completed");
-                    token.ThrowIfCancellationRequested();
-
-                    Debug.WriteLine("Step3:Processing...");
-                    Thread.Sleep(1000);
-                    Debug.WriteLine("Step3:Completed");
-                    token.ThrowIfCancellationRequested();
-
-                    Debug.WriteLine("Step4:Processing...");
-                    Thread.Sleep(1000);
-                    Debug.WriteLine("Step4:Completed");
-                    token.ThrowIfCancellationRequested();
-
-                    return "test.xlsx";
-                }
-                catch
-                {
-                    Debug.WriteLine("Cancel!!");
-                    return "";
-                }
-                finally
-                {
-                    //httpContext.Session.Remove("CancelToken");
-                }
+						return fileName;
+					}
+				}
+				catch
+				{
+					Debug.WriteLine("Cancel!!");
+					return "";
+				}
+				finally
+				{
+					//httpContext.Session.Remove("CancelToken");
+				}
 #endif
-            });
-        }
+			});
+		}
 
-        //private string AnalyzeAction<TViewModel>(HttpContext httpContext, TViewModel viewModel, CancellationToken token)
-        //{
-        //    // キャンセル判定
-        //    if (token.IsCancellationRequested) return "";
+		//private string AnalyzeAction<TViewModel>(HttpContext httpContext, TViewModel viewModel, CancellationToken token)
+		//{
+		//    // キャンセル判定
+		//    if (token.IsCancellationRequested) return "";
 
-        //    // データ取得
-        //    //var data = GetData(httpContext, viewModel);
+		//    // データ取得
+		//    //var data = GetData(httpContext, viewModel);
 
-        //    // データ解析
-        //    //AnalyzeData(data);
+		//    // データ解析
+		//    //AnalyzeData(data);
 
-        //    // 解析結果の作成
-        //    //return CreateResult(data);
+		//    // 解析結果の作成
+		//    //return CreateResult(data);
 
-        //    return "";
-        //}
+		//    return "";
+		//}
 
-        //protected abstract TResult GetData<TViewModel, TResult>(HttpContext httpContext, TViewModel viewModel);
-        //protected abstract void AnalyzeData<TData>(TData data);
-        //protected abstract string CreateResult<TData>(TData data);
+		//protected abstract TResult GetData<TViewModel, TResult>(HttpContext httpContext, TViewModel viewModel);
+		//protected abstract void AnalyzeData<TData>(TData data);
+		//protected abstract string CreateResult<TData>(TData data);
 
-        #region IDispose members
+		#region IDispose members
 
-        /// <summary>
-        /// このインスタンスによって使用されているリソースを全て破棄します。
-        /// </summary>
-        public void Dispose()
-        {
-            //this.Logger.Debug("AnalyzableServiceBase:Dispose");
-        }
+		/// <summary>
+		/// このインスタンスによって使用されているリソースを全て破棄します。
+		/// </summary>
+		public void Dispose()
+		{
+			//this.Logger.Debug("AnalyzableServiceBase:Dispose");
+		}
 
-        #endregion IDispose members
-    }
+		#endregion IDispose members
+	}
 }
