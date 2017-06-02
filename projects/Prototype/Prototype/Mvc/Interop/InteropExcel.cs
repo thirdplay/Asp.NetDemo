52c229b98b73d3c03f3344504c82eb0b6861c889
thirdplay <thirdplay@outlook.com>
2017-06-03 08:22:21 +0900

    interop検証

--- projects/Prototype/Prototype/Mvc/Interop/InteropExcel.cs
+++ projects/Prototype/Prototype/Mvc/Interop/InteropExcel.cs
@@ -100,6 +100,7 @@ namespace Prototype.Mvc.Interop
 				case 3: this.application.Run(macro, args[0], args[1], args[2]); break;
 				case 4: this.application.Run(macro, args[0], args[1], args[2], args[3]); break;
 				case 5: this.application.Run(macro, args[0], args[1], args[2], args[3], args[4]); break;
+				case 6: this.application.Run(macro, args[0], args[1], args[2], args[3], args[4], args[5]); break;
 			}
 		}
 
